9fea32451ba8693cadf928b656c0b72067fcacc9
ValentinRouge <rougevalentin@outlook.com>
2022-03-11 12:19:15 +0100

    V0 du header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./*/*.html","./*/*.php","./*/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'XBlueLight': '#daeaf6',
+        'XBlueMiddle': '#0b8abc',
+        'XBlueStrong': '#124e96',
+        'XPink': '#ff008e'
+      }
+    },
   },
   plugins: [],
 }
