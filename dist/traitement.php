27ba5da31aebd508fe18df763fad9ab9af112759
Legeai <gule60731@eleve.isep.fr>
2022-05-03 11:50:28 +0200

    page contact php je veux dead

--- dist/traitement.php
+++ dist/traitement.php
@@ -3,15 +3,25 @@
 <head>
     <meta charset="utf-8">
     <title>Envoie d'un mail via le formulaire</title>
+    <link rel="stylesheet" href="../css/contact.css" />
 </head>
 
     <body>
         <?php
-         if (isset($_POST['message'])) {
-            $retour = mail('guillaume.legeai@gmail.com','Page contact', $_POST['message'],'From: webmaster@monsite.fr')
-            if($retour)
-                echo '<p>votre message a bien été envoyé.</p>';
-            }
-        ?>
+         if(isset($_POST['mailform']))
+
+         $header="MIME-Version: 1.0\r\n";
+         $header.='From:"guillaume.legeai@gmail.com"<guillaume.legeai@gmail.com>'."\n";
+         $header.='Content-Type:text/html; charset="uft-8"'."\n";
+         $header.='Content-Transfer-Encoding: 8bit';
+         
+         $message='J\'ai envoyé ce mail avec PHP ';
+         
+         mail("guillaume.legeai@gmail.com", "retour xcolozoo!", $message, $header);
+         }
+         ?>
+         <form method="POST" action="">
+             <input type="submit" value="Recevoir un mail !" name="mailform"/>
+         </form>
     </body>
 </html>
