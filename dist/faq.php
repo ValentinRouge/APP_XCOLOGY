067c048a6e35cf242a493e333e21cc4f779859cc
ShuyuanZHENG35 <shzh60467@eleve.isep.fr>
2022-05-06 09:26:20 +0200

    faq.php

--- dist/faq.php
+++ dist/faq.php
@@ -1,5 +1,103 @@
-<?php 
-    include 'html/header.html';
-    include 'html/faq.html';
-    include 'html/footer.html';
-?>
+<!DOCTYPE html>
+<html lang = 'en'>
+<title>Foire aux questions</title>	
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" type="text/css" href="./css/faq.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+
+	
+</head>
+<body>
+	
+	<?php include 'html/header.html'; 
+            include 'connectionToBDD.php';
+            ?>
+	<div class="inside">
+		
+		<div class="faq_header">
+			<h1 class="Faq">Foire aux questions</h1>
+			<img src="../img/faq2.PNG" width="90px" height="90px" class="img">	
+			<img src="../img/faq.PNG" width="90px" height="90px" class="img" >
+			<img src="../img/faq3.PNG" width="90px" height="90px" class="img" >
+		</div>
+		<main>
+		<img src="./img/faq1.PNG" alt="faq"class="img1">
+			<div class="main_content">
+				<div class="question-answer">
+        			<div class="question"> 
+         				<h2 class="question">
+                        <?php
+                            include 'connectionToBDD.php';
+                            $requet = "SELECT question FROM FAQ";
+                            $result = $connection->query($requet);
+                            $row = $result->fetch_assoc();
+                            echo $row['question'];
+                        ?>  
+                        </h2>
+         				<button class="faq-button">
+          					<span class="up-icon">
+           						<i class="fa-solid fa-chevron-up"></i>
+          					</span>
+          					<span class="down-icon">
+           						<i class="fa-solid fa-chevron-down"></i>
+          					</span>
+         				</button> 
+        			</div>
+        			<div class="answer">
+         				<p class="answer">
+                        <?php
+                            include 'connectionToBDD.php';
+                            $requet = "SELECT answer FROM FAQ";
+                            $result = $connection->query($requet);
+                            $row = $result->fetch_assoc();
+                            echo $row['answer'];
+                        ?>
+                        </p>
+     
+        			</div>
+                </div>
+                <div class="question-answer">
+        <div class="question"> 
+         <h2 class="question">
+                        <?php
+                            include 'connectionToBDD.php';
+                            $requet = "SELECT question FROM FAQ";
+                            $result = $connection->query($requet);
+                            $row = $result->fetch_assoc();
+                            echo $row['question'];
+                        ?>  
+                        </h2>
+         <button class="faq-button">
+          <span class="up-icon">
+           <i class="fa-solid fa-chevron-up"></i>
+          </span>
+          <span class="down-icon">
+           <i class="fa-solid fa-chevron-down"></i>
+          </span>
+         </button> 
+        </div>
+        <div class="answer">
+         <p class="answer">
+                        <?php
+                            include 'connectionToBDD.php';
+                            $requet = "SELECT answer FROM FAQ";
+                            $result = $connection->query($requet);
+                            $row = $result->fetch_assoc();
+                            echo $row['answer'];
+                        ?>
+                        </p>
+     
+        </div>
+                </div>
+	
+                
+            </div>            
+		</main>
+	</div>	
+	<script src="../js/faq.js"></script>	
+    <?php include 'html/footer.html'; ?>
+</body>
+</html>
