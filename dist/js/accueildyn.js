c530f0436db8faa894b628f183cc2c7ca73fcd09
styx607 <styx607@gmail.com>
2022-05-06 12:43:11 +0200

    modif homepage et ajout accueildyn.js

---
+++ dist/js/accueildyn.js
@@ -0,0 +1,13 @@
+const titreSpans = document.querySelectorAll('h1 span');
+const btns = document.querySelectorAll('.btn-first');
+
+window.addEventListener('load', () => {
+
+    const TL = gsap.timeline({paused: true});
+
+    TL
+    .staggerFrom(titreSpans, 1, {top: -50, opacity: 0, ease: "power2.out"}, 0.3)
+    .staggerFrom(btns, 1, {opacity: 0, ease: "power2.out"}, 0.3, '-=1')
+
+    TL.play();
+})
