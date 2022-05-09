0f32455e081f8b4fa17f87c61157635b8c130408
Legeai <gule60731@eleve.isep.fr>
2022-05-09 17:48:49 +0200

    redesign contact

---
+++ dist/js/contact.js
@@ -0,0 +1,10 @@
+
+let date = document.querySelector('.calendrier');
+let heure =document.querySelector('.horloge');
+let texte= document.querySelector('#texte')
+
+function datechange (){
+    date.setAttribute('src',"../img/Calendrier2.png")
+    date.style.opacity="O.5"
+}
+
