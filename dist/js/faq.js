7e779538fe26b412400564d1a66908d4dec736cf
ShuyuanZHENG35 <shzh60467@eleve.isep.fr>
2022-05-13 09:01:47 +0200

    faq.js

--- dist/js/faq.js
+++ dist/js/faq.js
@@ -1,12 +1,17 @@
+window.addEventListener("DOMContentLoaded", (event)=> {
 const questions = document.querySelectorAll('.question-answer');
  
-questions.forEach(function(question){
-    console.log(question ); 
-    const btn = question.querySelector('.faq-button');
-    console.log(btn);
-    btn.addEventListener("click",function(item){
-
-        question.classList.toggle("show-text");
-        
+    questions.forEach(function(question){
+        console.log(question ); 
+        const btn = question.querySelector('.faq-button');
+        console.log(btn);
+        btn.addEventListener("click",function(){
+    
+            question.classList.toggle("show-text");
+            
+        })
     })
-})
+})  
+
+
+
