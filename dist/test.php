8d7dfefb364222f501896d882a199a9a29e7a164
ValentinRouge <rougevalentin@outlook.com>
2022-05-23 14:14:51 +0200

    test mail

--- dist/test.php
+++ dist/test.php
@@ -1,10 +1,5 @@
 <?php
 
-$pass = password_hash("1234", PASSWORD_DEFAULT);
-echo $pass;
-echo "\n";
-if (password_verify("1234","0$2y$10\$fDnTDv1SwQV3f/VU2H2Bjua9ShVu4bwR3Ak1hwATh33G.KY88U0dO")) echo "yes";
-if (password_verify("1234",$pass)) echo "yes2";
-
+mail('rougevalentin@icloud.com', "test", "sujet");
 
 ?>
