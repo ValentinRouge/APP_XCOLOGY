6a8afa268b0b18bc49b870c1b6fcffd82aaf8ebe
styx607 <styx607@gmail.com>
2022-05-29 18:11:36 +0200

    inscr back + hompg act

--- dist/inscription.php
+++ dist/inscription.php
@@ -12,21 +12,24 @@
         include 'header.php' ; 
     ?>
     
+    <center>
     <fieldset>
             
         <form method="POST" action="admin-inscription.php">
+            <h1>Inscrivez-Vous !</h1>
+
             <hr>
-            <div class="SignIn-box">
-                <div class="textbox">
+            <table>
+
                     <input type="text" name="pseudo" id="pseudo" placeholder="Votre peusdo" required><br/>
                     <input type="text" name="nom" id="nom" placeholder="Votre nom" required><br/>
                     <input type="text" name="prenom" id="prenom" placeholder="Votre prenom" required><br/>
                     <input type="email" name="username" id="username" placeholder="Votre email" required><br/>
                     <input type="password" name="password" id="password" placeholder="Votre mot de passe" required><br/>
                     <input type="password" name="password2" id="password2" placeholder="Confirmez votre mot de passe" required><br/>
-                    <input type="button" class="btn" value=" S'inscrire">
-                </div>
-            </div>
+                    <input type="submit" name="envoie" id="envoie" value=" S'inscrire">
+
+            </table>
         </form>
         <?php
             if(isset($_GET['erreur'])){
@@ -36,7 +39,7 @@
             }
         ?>
     </fieldset>
-    
+    </center>
 
     <?php
         include 'html/footer.html';
