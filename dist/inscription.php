3d854dcf3601668c314a2576c20971324e7a2043
styx607 <styx607@gmail.com>
2022-05-29 13:46:03 +0200

    inc et home

--- dist/inscription.php
+++ dist/inscription.php
@@ -17,8 +17,7 @@
         <form method="POST" action="admin-inscription.php">
             <hr>
             <div class="SignIn-box">
-                <h1>Inscription</h1>
-                <table class="textbox">
+                <div class="textbox">
                     <input type="text" name="pseudo" id="pseudo" placeholder="Votre peusdo" required><br/>
                     <input type="text" name="nom" id="nom" placeholder="Votre nom" required><br/>
                     <input type="text" name="prenom" id="prenom" placeholder="Votre prenom" required><br/>
@@ -26,7 +25,7 @@
                     <input type="password" name="password" id="password" placeholder="Votre mot de passe" required><br/>
                     <input type="password" name="password2" id="password2" placeholder="Confirmez votre mot de passe" required><br/>
                     <input type="button" class="btn" value=" S'inscrire">
-                </table>
+                </div>
             </div>
         </form>
         <?php
