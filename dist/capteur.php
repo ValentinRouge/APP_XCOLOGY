95a7d5a1440e793544133c8664290412c752d6f3
styx607 <styx607@gmail.com>
2022-05-31 12:26:01 +0200

    fondu titre capteurs

--- dist/capteur.php
+++ dist/capteur.php
@@ -110,8 +110,9 @@
         
 
         <?php include 'html/footer.html'?>
-        
+
         <script src="./js/capteur.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
         <script> scr="../js/capteursdyn.js"</script>
 
     </body>
