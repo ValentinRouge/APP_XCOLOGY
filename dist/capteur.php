bdd95ab1c6021e12b520ace4ee1fc794c124455a
styx607 <styx607@gmail.com>
2022-05-31 12:14:31 +0200

    fondu

--- dist/capteur.php
+++ dist/capteur.php
@@ -17,6 +17,7 @@
 
         <title>Capteur zone singe</title>
         <link rel="stylesheet" href="css/capteurs.css">
+        <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     </head>
     <body class="bg-XBlueLight">
         <?php 
